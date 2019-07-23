697fa7cc7fcf22eb43c04c6b8dc7086d536de182
Vaibhav <35535347+VaibhavZi@users.noreply.github.com>
2019-07-23 09:27:23 +0530

    Added new project structure
    Account Service and Balance Service prject structure

---
+++ bank-balance-service/src/main/java/com/vz/bankbalanceservice/BankBalanceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vz.bankbalanceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankBalanceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankBalanceServiceApplication.class, args);
+    }
+
+}
